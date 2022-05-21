d69313a0cc4cf32581a1cafda461a38ffac39849
Efrain Hernandez <efrainhg2050@gmail.com>
2022-05-20 23:21:11 -0500

    Work with numbers in TS

---
+++ bases/tipos/numbers.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let avengers = 10;
+    const villians = 20;
+    if (avengers < villians) {
+        console.log("Estamos en problemas");
+    }
+    else {
+        console.log("Nos Salvamos");
+    }
+    avengers = Number();
+    console.log({ avengers });
+})();
