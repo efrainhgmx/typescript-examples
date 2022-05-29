7b0bad5554eb935f44ad34ccf836e1b387d1de2a
Efrain Hernandez <efrainhg2050@gmail.com>
2022-05-28 19:46:08 -0500

    types in functions

---
+++ bases/funciones/function-type.js
@@ -0,0 +1,17 @@
+"use strict";
+(() => {
+    const addNumber = (a, b) => a + b;
+    const greet = (name) => `Hola ${name}`;
+    const saveTheWorld = () => `El mundo esta salvado`;
+    let myFunction;
+    let mySecondFunction;
+    let myThirdFunction;
+    /* myFunction = 10;
+    console.log(myFunction); */
+    myFunction = addNumber;
+    console.log(myFunction(1, 2));
+    mySecondFunction = greet;
+    console.log(mySecondFunction('Clark'));
+    myThirdFunction = saveTheWorld;
+    console.log(myThirdFunction());
+})();
