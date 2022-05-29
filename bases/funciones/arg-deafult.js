a2a06ba6568bf9f07bcdd13bc6146f3877332d6e
Efrain Hernandez <efrainhg2050@gmail.com>
2022-05-28 19:18:35 -0500

    Descripton add for default args in functions

--- bases/funciones/arg-deafult.js
+++ bases/funciones/arg-deafult.js
@@ -1,5 +1,7 @@
 "use strict";
 (() => {
+    //Los valores requeridos, NO pueden ir despues de un opcional
+    //A menos que se inicialice por defecto
     const fullName = (firstName, lastName, upper = false) => {
         if (upper) {
             return `${firstName.toLocaleUpperCase()} ${lastName === null || lastName === void 0 ? void 0 : lastName.toLocaleUpperCase()}`;
--- bases/funciones/arg-deafult.ts
+++ bases/funciones/arg-deafult.ts
@@ -1,4 +1,7 @@
 (() => {
+
+    //Los valores requeridos, NO pueden ir despues de un opcional
+    //A menos que se inicialice por defecto
     const fullName = (firstName: string, lastName?: string, upper:boolean = false): string => {
         if(upper) {
             return `${firstName.toLocaleUpperCase()} ${lastName?.toLocaleUpperCase()}`;
