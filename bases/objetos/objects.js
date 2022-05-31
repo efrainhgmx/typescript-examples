24b14001c74be7c505b7bf562bd57ca339b69dee
Efrain Hernandez <efrainhg2050@gmail.com>
2022-05-31 18:53:45 -0500

    working with objects

---
+++ bases/objetos/objects.js
@@ -0,0 +1,17 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super Velocidad', 'Viajar en el tiempo']
+    };
+    flash = {
+        name: 'Otro nombre',
+        age: 25,
+        powers: ['Volar', 'Cantar'],
+        getNombre() {
+            return this.name;
+        }
+    };
+    console.log(flash);
+})();
