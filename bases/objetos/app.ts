add5b0b9cf62d34065921ca8835051ac91c3f150
Efrain Hernandez <efrainhg2050@gmail.com>
2022-05-31 19:49:07 -0500

    Challenge done

---
+++ bases/objetos/app.ts
@@ -0,0 +1,67 @@
+
+// Objetos
+const batimovil: {carroceria: string, modelo:string, antibalas: boolean, pasajeros: number} = {
+  carroceria: "Negra",
+  modelo: "6x6",
+  antibalas: true,
+  pasajeros:4
+};
+
+const bumblebee:{carroceria: string, modelo: string, antibalas: boolean, pasajeros: number, disparar?: () => void} = {
+  carroceria: "Amarillo con negro",
+  modelo: "4x2",
+  antibalas: true,
+  pasajeros:4,
+  disparar(){ // El metodo disparar es opcional
+    console.log("Disparando");
+  }
+};
+
+
+// Villanos debe de ser un arreglo de objetos personalizados
+type Villian = {
+  nombre: string;
+  edad: number | undefined;
+  mutante: boolean;
+}
+const villanos: Villian[] = [{
+  nombre:"Lex Luthor",
+  edad: 54,
+  mutante:false
+},{
+  nombre: "Erik Magnus Lehnsherr",
+  edad: 49,
+  mutante: true
+},{
+  nombre: "James Logan",
+  edad: undefined,
+  mutante: true
+}];
+
+// Multiples tipos
+// cree dos tipos, uno para charles y otro para apocalipsis
+type Charles = {
+  poder: string;
+  estatura: number;
+}
+
+type Apocalipsis = {
+  lider: boolean;
+  miembros: string[]
+}
+
+const charles: Charles = {
+  poder:"psiquico",
+  estatura: 1.78
+};
+
+const apocalipsis: Apocalipsis = {
+  lider:true,
+  miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+}
+
+// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+let mystique: (Charles | Apocalipsis);
+
+mystique = charles;
+mystique = apocalipsis;
