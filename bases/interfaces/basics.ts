4c41409fd174faa75445e5a0dbdea1b45cec9d39
Efrain Hernandez <efrainhg2050@gmail.com>
2022-06-16 21:25:10 -0500

    Little differents bettwen interface and type

--- bases/interfaces/basics.ts
+++ bases/interfaces/basics.ts
@@ -1,3 +1,30 @@
 (() => {
+    //A diferencia de Type, las interfaces pueden ser extendibles y los types no
+    //Type no pueden expanderse más pero en escencia son lo mismo
+    interface Hero {
+        name: string;
+        age?: number;
+        powers: string[];
+        getName: () => string;
+    }
+
+    let flash:Hero = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super Velocidad', 'Viajar en el tiempo'],
+        getName() {
+            return this.name;
+        }
+    }
+
+    let superman:Hero = {
+        name: 'Clark Ken',
+        age: 32,
+        powers: ['Super velocidad', 'Super Fuerza'],
+        getName() {
+            return this.name;
+        }
+    }
+
 
 })();
--- bases/main.js
+++ bases/main.js
@@ -1,4 +1,20 @@
 "use strict";
 (() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super Velocidad', 'Viajar en el tiempo'],
+        getName() {
+            return this.name;
+        }
+    };
+    let superman = {
+        name: 'Clark Ken',
+        age: 32,
+        powers: ['Super velocidad', 'Super Fuerza'],
+        getName() {
+            return this.name;
+        }
+    };
 })();
 //# sourceMappingURL=main.js.map
