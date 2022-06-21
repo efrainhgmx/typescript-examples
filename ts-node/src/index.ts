97be51296723404feadca7753b7885a1572e90d2
Efrain Hernandez <efrainhg2050@gmail.com>
2022-06-21 13:34:37 -0500

    using a generic function

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,14 +1,20 @@
 import { printObject, genericFunction } from './generics/generics';
-printObject(123);
+import { Hero } from './interfaces/hero';
+import { Villian } from './interfaces/villian';
+/* printObject(123);
 printObject(new Date());
 printObject([1,2,3]);
 printObject({a: 'a'})
 
 console.log(genericFunction(3.14).toFixed(2));
-console.log(genericFunction('Hola mundo').length);
+console.log(genericFunction('Hola mundo').length); */
 
 
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Wilson',
+    dangerLevel: 130
+}
 
-
-
-
+//Generic function
+console.log(genericFunction<Villian>(deadpool).dangerLevel)
